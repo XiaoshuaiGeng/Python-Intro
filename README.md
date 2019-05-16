17c3e87f5953090a17e34fc9f07946a0cf42d70b
Xiaoshuai Geng <genglittlehandsome@hotmail.com>
2019-05-16 19:21:10 -0400

    Python exercise

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Python-basic
+<h2>Self-learning python from online</h2>
+<p>Some of the python exercise will be post here.</p>
---
+++ test.py
@@ -0,0 +1,7 @@
+
+#String format exercise
+result = 100 / 777
+print(result)
+print("{}".format("Hello World"))
+#print(f'hello world{result}')
+#The line above is a comment because the f-string methond is supported in python 3.6 or higher
