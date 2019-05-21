a088243b45dae9c66e437e061d7b9cae57f44097
Xiaoshuai Geng <genglittlehandsome@hotmail.com>
2019-05-21 12:58:13 -0400

    further editing

--- test.py
+++ test.py
@@ -1,7 +1,16 @@
 
 #String format exercise
+print('String format exercise')
 result = 100 / 777
 print(result)
 print("{}".format("Hello World"))
 #print(f'hello world{result}')
 #The line above is a comment because the f-string methond is supported in python 3.6 or higher
+print('****************\n')
+#list testing
+print('List testing')
+print("my_list1 is [1,2,3]")
+print("my_list2 is ['String',100,23,2]")
+my_list1 = [1,2,3]
+my_list2 = ['String',100,23,2]
+print('my_list2 is {}'.format(len(my_list2)))
