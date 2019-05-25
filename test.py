037d83fb2cea492444639c89529a7136b8f59304
Xiaoshuai Geng <genglittlehandsome@hotmail.com>
2019-05-25 16:55:31 -0400

    Update test.py

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ result = 100 / 777
 print(result)
 print("{}".format("Hello World"))
 # print(f'hello world{result}')
-# The line above is a comment because the f-string methond is supported in python 3.6 or higher
+# The line above is a comment because the f-string method is supported in python 3.6 or higher
 print('****************\n')
 # list testing
 print('List testing')
