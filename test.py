188f04633cf3897b29751c48924c1244d19c168a
Xiaoshuai Geng <genglittlehandsome@hotmail.com>
2019-05-25 17:26:50 -0400

    f-string format is solved

--- test.py
+++ test.py
@@ -2,10 +2,11 @@
 # String format exercise
 print('String format exercise')
 result = 100 / 777
+testString = "test"
 print(result)
 print("{}".format("Hello World"))
-# print(f'hello world{result}')
-# The line above is a comment because the f-string method is supported in python 3.6 or higher
+print(f'hello world{testString}')
+# f-string method is supported in python 3.6 or higher
 print('****************\n')
 # list testing
 print('List testing')
@@ -13,6 +14,7 @@ print("my_list1 is [1,2,3]")
 print("my_list2 is ['String',100,23,2]")
 my_list1 = [1,2,3]
 my_list2 = ['String',100.23,2]
+# len() measures the length of the string
 print('my_list2 is {}'.format(len(my_list2)))
 print('This element is from my_list1[2]: {}'.format(my_list1[2]))
 print('This element is from my_list2[1]: {}'.format(my_list2[1]))
@@ -21,3 +23,4 @@ print("String appending")
 print('my_list1.append(4)')
 my_list1.append(4)
 print('This element is from my_list1[2]: {}'.format(my_list1))
+
