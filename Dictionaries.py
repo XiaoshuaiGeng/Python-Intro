6e7f7db25a258732c96ba1974573cba154d05a5b
Xiaoshuai Geng <genglittlehandsome@hotmail.com>
2019-06-05 10:52:26 -0400

    Create Dictionaries.py

---
+++ Dictionaries.py
@@ -0,0 +1,13 @@
+# Dictionaries
+# Dictionary functions
+
+# 1. keys()
+d = {'k1':100,'k2':200,'k3':300}
+print('The value of the dictionary d is {}'.format(d))
+print(d.keys())
+
+# 2. values()
+print(d.values())
+
+# 3. items()
+print(d.items())
