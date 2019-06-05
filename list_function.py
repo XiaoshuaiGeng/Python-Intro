d705dd060adb8e9f238fb1d4a9aff43ad2ed0dc3
Xiaoshuai Geng <genglittlehandsome@hotmail.com>
2019-06-05 10:14:49 -0400

    Update code

--- list_function.py
+++ list_function.py
@@ -1,4 +1,4 @@
-#List Practice
+# List Practice
 # 1. List is initialized in middle brackets []
 
 list = ['one', 'two', 'three']
@@ -9,3 +9,8 @@ list.append('four')
 print(list)
 
 # 3. List pop()
+list.pop()  # pop() will pop the last index element in default
+print(list)
+list.append('five')  # list concatenation also works list + ['five']
+print('Print the full list {}'.format(list))
+print('Print the list from its first index {}'.format(list[1:]))
