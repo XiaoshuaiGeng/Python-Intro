80a571cb7402f82911366abd88ac4afc6bc408ba
Xiaoshuai Geng <genglittlehandsome@hotmail.com>
2019-06-13 10:59:15 -0400

    Update code

---
+++ Section3/File_IO.py
@@ -0,0 +1,16 @@
+# File I/O
+# File functions
+# For Windows the '\' is escaped using '\\'
+# For MacOS the '\', use '/' instead
+# Text in the file is "This is a test"
+
+# open()
+file = open('fileload.txt', mode='r')
+
+# seek()
+file.seek(2)
+
+# read() / readlines()
+print(file.read())
+file.seek(0)
+print(file.readlines())
